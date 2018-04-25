52f5d993bd91a08745df7be00063ea912080c20a
Chenyatog0129 <1137583550@qq.com>
2018-04-25 14:25:56 +0800

    修改cyt

--- MyQuarter/app/src/main/java/weizhihui/bwie/com/myquarter/Fragment/Fragment01.java
+++ MyQuarter/app/src/main/java/weizhihui/bwie/com/myquarter/Fragment/Fragment01.java
@@ -1,16 +1,20 @@
 package weizhihui.bwie.com.myquarter.Fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.LinearLayout;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,21 +24,15 @@ import weizhihui.bwie.com.myquarter.R;
  * Created by asus on 2018/4/16.
  */
 
-public class Fragment01 extends Fragment implements View.OnClickListener {
+public class Fragment01 extends Fragment {
 
+
+    private String[] tabs = {"热门", "关注"};
     private View view;
-
+    private TabLayout mTabid;
     private ViewPager mPager;
-    /**
-     * 热门
-     */
-    private RadioButton mBtn1;
-    /**
-     * 关注
-     */
-    private RadioButton mBtn2;
-    private RadioGroup mGroup;
-
+    private MainFragment mainFragment;
+    private List<Fragment> fragmentList;
 
     @Nullable
     @Override
@@ -42,89 +40,82 @@ public class Fragment01 extends Fragment implements View.OnClickListener {
 
         View view = inflater.inflate(R.layout.tuijian, container, false);
         initView(view);
+        mTabid.setupWithViewPager(mPager);
+        for (int i=0;i<tabs.length;i++){
+            mTabid.addTab(mTabid.newTab().setText(tabs[i]));
+        }
+
+        mTabid.post(new Runnable() {
+            @Override
+            public void run() {
+                setIndicator(mTabid,60,60);
+            }
+        });
 
 
-        final List<Fragment> list = new ArrayList<>();
-        list.add(new Frag_pager01());
-        list.add(new Frag_pager02());
-
-        mPager.setAdapter(new FragmentPagerAdapter(getFragmentManager()) {
+        fragmentList = new ArrayList<>();
+        for (int i=0;i<tabs.length;i++){
+            mainFragment = new MainFragment();
+            Bundle bundle=new Bundle();
+            bundle.putString("tab",tabs[i]);
+            mainFragment.setArguments(bundle);
+            fragmentList.add(mainFragment);
+        }
+        //viewpager添加适配器
+        FragmentManager supportFragmentManager = getActivity().getSupportFragmentManager();
+        mPager.setAdapter(new FragmentPagerAdapter(supportFragmentManager) {
             @Override
             public Fragment getItem(int position) {
-                return list.get(position);
+                return fragmentList.get(position);
             }
 
             @Override
             public int getCount() {
-                return list.size();
+                return fragmentList.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return tabs[position];
             }
         });
-
-        mPager.setCurrentItem(0);
-
-        mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position){
-                    case 0 :
-                        mGroup.check(R.id.btn1);
-                        break;
-                    case 1:
-                        mGroup.check(R.id.btn2);
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        mGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.btn1 :
-
-                        mPager.setCurrentItem(0);
-                        break;
-                    case R.id.btn2 :
-                        mPager.setCurrentItem(1);
-                        break;
-                }
-            }
-        });
-
-
         return view;
     }
 
+
     private void initView(View view) {
-
+        mTabid = (TabLayout) view.findViewById(R.id.tabid);
         mPager = (ViewPager) view.findViewById(R.id.pager);
-
-        mBtn1 = (RadioButton) view.findViewById(R.id.btn1);
-        mBtn1.setOnClickListener(this);
-        mBtn2 = (RadioButton) view.findViewById(R.id.btn2);
-        mBtn2.setOnClickListener(this);
-        mGroup = (RadioGroup) view.findViewById(R.id.Group);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            default:
-                break;
-            case R.id.btn1:
-                break;
-            case R.id.btn2:
-                break;
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        tabStrip.setAccessible(true);
+        LinearLayout llTab = null;
+        try {
+            llTab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+        for (int i = 0; i < llTab.getChildCount(); i++) {
+            View child = llTab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
         }
     }
 }
