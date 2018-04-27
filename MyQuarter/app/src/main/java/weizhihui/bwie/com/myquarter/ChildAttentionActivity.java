02d9683069c31ea4e10ed09393a82f8fbd0ed746
Chenyatog0129 <1137583550@qq.com>
2018-04-27 16:09:39 +0800

    cyt1

--- MyQuarter/app/src/main/java/weizhihui/bwie/com/myquarter/ChildAttentionActivity.java
+++ MyQuarter/app/src/main/java/weizhihui/bwie/com/myquarter/ChildAttentionActivity.java
@@ -3,7 +3,9 @@ package weizhihui.bwie.com.myquarter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +13,14 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.controller.AbstractDraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
-public class ChildAttentionActivity extends AppCompatActivity {
+import java.util.List;
+
+import weizhihui.bwie.com.myquarter.adapter.Remen_Adapter;
+import weizhihui.bwie.com.myquarter.bean.Remen_Bean;
+import weizhihui.bwie.com.myquarter.presenter.Remen_Presenter;
+import weizhihui.bwie.com.myquarter.view.Remen_View;
+
+public class ChildAttentionActivity extends AppCompatActivity implements Remen_View{
 
     private ImageView mACimgid;
     private ImageView mACimg2id;
@@ -35,6 +44,18 @@ public class ChildAttentionActivity extends AppCompatActivity {
                 .build();
         mACSimpleId.setController(build);
 
+        Remen_Presenter remen_presenter = new Remen_Presenter(this);
+        remen_presenter.getRemen_presenter(1,1,1,"android",101);
+           //添加布局管理器
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        mCArvid.setLayoutManager(linearLayoutManager);
+            //点击返回  返回上一个页面
+        mACimgid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private void initView() {
@@ -44,5 +65,12 @@ public class ChildAttentionActivity extends AppCompatActivity {
         mACfenid = (TextView) findViewById(R.id.ACfenid);
         mACimgGuanid = (ImageView) findViewById(R.id.ACimgGuanid);
         mCArvid = (RecyclerView) findViewById(R.id.CArvid);
+    }
+
+    @Override
+    public void setRemen_View(Remen_Bean remen_bean) {
+        List<Remen_Bean.DataEntity> data = remen_bean.getData();
+        Remen_Adapter remen_adapter = new Remen_Adapter(ChildAttentionActivity.this, data);
+        mCArvid.setAdapter(remen_adapter);
     }
 }
---
+++ MyQuarter/app/src/main/java/weizhihui/bwie/com/myquarter/SettingActivity.java
@@ -0,0 +1,87 @@
+package weizhihui.bwie.com.myquarter;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * 版本号
+     */
+    private TextView mVersionCodeid;
+    /**
+     * 退出登录
+     */
+    private Button mUnLogin;
+    private int versionCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        initView();
+
+        String appVersionName = getAppVersionName(this);
+        mVersionCodeid.setText(appVersionName);
+    }
+
+    private void initView() {
+        mVersionCodeid = (TextView) findViewById(R.id.versionCodeid);
+        mUnLogin = (Button) findViewById(R.id.un_login);
+        mUnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            //退出登录
+            case R.id.un_login:
+                finish();
+                break;
+        }
+    }
+
+    //获取当前的版本号
+    private int getVersionCode() {
+        //获取packagemanager的实例
+        PackageManager packageManager = getPackageManager();
+        //getPackageName()是你当前类的包名，0代表是获取版本信息
+        PackageInfo packInfo = null;
+        try {
+            packInfo = packageManager.getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        Log.e("TAG","版本号"+packInfo.versionCode);
+        Log.e("TAG","版本名"+packInfo.versionName);
+        return packInfo.versionCode;
+    }
+
+    /**
+     * 返回当前程序版本名
+     */
+    public static String getAppVersionName(Context context) {
+        String versionName = "";
+        try {
+            // ---get the package info---
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            versionName = pi.versionName;
+            if (versionName == null || versionName.length() <= 0) {
+                return "";
+            }
+        } catch (Exception e) {
+            Log.e("VersionInfo", "Exception", e);
+        }
+        return versionName;
+    }
+}
